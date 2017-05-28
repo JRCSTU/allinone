df77f3f57f7b0be8575c3d694091f69adbe2be4a
Kostis Anagnostopoulos <ankostis@gmail.com>
2017-05-28 19:33:13 +0200

    feat(build): Add `-n` flag in `clean.sh`

--- clean.sh
+++ clean.sh
@@ -2,14 +2,22 @@
 my_dir=`dirname "$0"`
 cd $my_dir
 
+rm_opts="-vrf"
+
+if [[ " $* " =~ " -n " ]]; then
+    rm_cmd="echo PRETEND rm $rm_opts"
+else
+    rm_cmd="rm $rm_opts"
+fi
+
 ls  -d ./*AIO*/install.log \
     ./*AIO*/Apps/clink/profile/.history \
     ./*AIO*/Apps/WinPython/settings/.matplotlib \
     ./*AIO*/CO2MPAS/{.ipython,.ptpython,.matplotlib,.jupyter,\
 tutorial,clink,co2mpas.log,*xls*,*.zip,inp,out,.python_history,*.ipynb,.ipynb_checkpoints,\
 nodes_modules,.exe,.lesshst,.viminfo,.co2dice/*} \
-    | xargs rm -vrf
+    | xargs $rm_cmd
 
 ## TOO BIG.
-rm -vrf ./*AIO*/Apps\WinPython/python-3.5.2.amd64/Lib/site-packages/wltp/test
-find . -name __pycache__ -type d | xargs rm -vrf
+$rm_cmd ./*AIO*/Apps\WinPython/python-3.5.2.amd64/Lib/site-packages/wltp/test
+find . -name __pycache__ -type d | xargs $rm_cmd
