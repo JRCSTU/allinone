b9698675d42d29eabf27dcfebc7b545ca368ce97
Kostis Anagnostopoulos <ankostis@gmail.com>
2017-07-10 14:43:16 +0200

    fix(clean): white-list fils to (not) delete in CO2MPAS-home filder

--- clean.sh
+++ clean.sh
@@ -11,21 +11,16 @@ else
 fi
 
 aio=(./co2mpas_AIO*)  # array but... https://unix.stackexchange.com/questions/213812/
+find ${aio}/{*.xlsx,*.zip,*.ipynb} | xargs $rm_cmd
+find ${aio}/CO2MPAS/*  -mindepth 1 -depth -type d | grep -vFf keepfiles.txt | xargs $rm_cmd
+find ${aio}/CO2MPAS/*  -mindepth 1 | grep -vFf keepfiles.txt | xargs $rm_cmd
 
-ls  -d ${aio}/{install.log,*xlsx} \
-    ${aio}/Apps/clink/profile/.history \
-    ${aio}/Apps/WinPython/settings/.matplotlib \
-    ${aio}/CO2MPAS/{.ipython,.ptpython,.matplotlib,.jupyter,\
-    tutorial,clink,co2mpas.log,*xls*,*.zip,inp,out,.python_history,*.ipynb,.ipynb_checkpoints,\
-    nodes_modules,.exe,.lesshst,.viminfo,.co2dice,my_TS_name_key1.pub,keydefs.txt,\
-    inputs,.cache,.config,.local} \
-    | xargs $rm_cmd
 
 ## delete ankostis's key (in case...)
 gpg="${aio}/Apps/GnuPG/pub/gpg2"
 mykey="5006137DE2F6FEDDC702DBE69CF277C40A8A1B08"
-$gpg --batch --delete-key "$mykey"
-$gpg --batch --delete-secret-key "$mykey"
+$gpg --batch --delete-secret-keys "$mykey"
+$gpg --batch --delete-keys "$mykey"
 
 ## Ensure log-file not in DEBUG mode.
 sed -i 's/^    level: .*/    level: INFO  # one of: DEBUG INFO WARNING ERROR FATAL/' ${aio}/CO2MPAS/.co2_logconf.yaml
