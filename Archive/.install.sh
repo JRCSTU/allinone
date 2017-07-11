eaf0d971315d81a5200583b1560540ae5ee888cc
Kostis Anagnostopoulos <ankostis@gmail.com>
2017-07-11 19:49:37 +0200

    feat(install): Add HOME link, add _ in link-names

--- Archive/.install.sh
+++ Archive/.install.sh
@@ -13,20 +13,20 @@ echo "$mywdir"
 app_ver="$(co2mpas -V)"
 app_ver="${app_ver#co2mpas-}"
 app_group="CO2MPAS\\CO2MPAS-$app_ver"
-apps_folder="`cygpath -aw "$APPDATA"`"
+apps_folder="$(cygpath -aw "$APPDATA")"
 
 mkshortcut="wscript .mkshortcut.vbs"
 menus_folder="$apps_folder\\Microsoft\\Windows\\Start Menu\\Programs"
 mymenu_folder="$menus_folder\\$app_group"
 
-echo -e "`date`: Installing shortcuts --> '$mymenu_folder'"
+echo -e "$(date): Installing shortcuts --> '$mymenu_folder'"
 
 rm -rf "$mymenu_folder"
 mkdir -p "$mymenu_folder"
 
 ## GUI
 #
-$mkshortcut "$mymenu_folder\\CO2MPAS-$app_ver" \
+$mkshortcut "$mymenu_folder\\CO2MPAS_GUI-$app_ver" \
     "${mywdir}CO2MPAS.vbs" \
     /desc:"Runs CO2MPAS GUI." \
     /workingdir:"${mywdir}CO2MPAS" \
@@ -35,10 +35,20 @@ $mkshortcut "$mymenu_folder\\CO2MPAS-$app_ver" \
 
 ## Consoles
 #
-$mkshortcut "$mymenu_folder\\CO2MPAS CONSOLE-$app_ver" \
+$mkshortcut "$mymenu_folder\\CO2MPAS_CONSOLE-$app_ver" \
     "${mywdir}CONSOLE.vbs" \
     /desc:"Opens a console with CO2MPAS environment appropriately setup." \
     /workingdir:"${mywdir}CO2MPAS" \
+    /icon:"${mywdir}Apps\\CO2MPAS_logo.ico" \
+    /show:min
+
+## HOME
+#
+home="$(cygpath -aw "$HOME")"
+$mkshortcut "$mymenu_folder\\CO2MPAS_HOME-$app_ver" \
+    "${home}" \
+    /desc:"Opens a CO2MPAS home folder." \
+    /workingdir:"${home}" \
     /icon:"${mywdir}Apps\\CO2MPAS_logo.ico" \
     /show:min
 
