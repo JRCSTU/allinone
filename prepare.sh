52e85e7a9076824dd7534cbf67df9032b5fd77d3
Kostis Anagnostopoulos <ankostis@gmail.com>
2017-07-27 09:08:58 +0300

    feat(prep): test-key EXPIRATION on prepare.sh, manually inspect keys

--- prepare.sh
+++ prepare.sh
@@ -37,20 +37,6 @@ find ${aio}/{*.xlsx,*.zip,*.ipynb} | xargs $rm -rf
 find ${aio}/CO2MPAS  -mindepth 1 | grep -vFf keepfiles.txt | xargs $rm -rf
 find ${aio}/Apps/WinPython/settings -mindepth 1  | grep -v winpython.ini | grep -v .jupyter | grep -v .ipython | xargs $rm -rf
 
-## delete ankostis's key (in case...)
-test_key="5464E04EE547D1FEDCAC4342B124C999CBBB52FF"
-stamper_key="4B12BCD5788511063B543190E09DF306"
-keys="$($GPG --allow-weak-digest-algos --list-public-keys --fingerprint --with-colons |
-        grep fpr |
-        grep -v $stamper_key | grep -v $test_key |
-        cut -d: -f10 )"
-
-echo "Deleting keys: ($keys)"
-for key in $keys; do
-    $gpg --batch --delete-secret-keys "$key"
-    $gpg --batch --delete-keys $key
-done
-
 ## TOO BIG.
 $rm -rf ${aio}/Apps\WinPython/python-3.5.2.amd64/Lib/site-packages/wltp/test
 find . -name __pycache__ -type d | xargs $rm -rf
@@ -71,3 +57,13 @@ $mkdir -p "$aio/Apps/GnuPG/var/cache/gnupg"
 
 ## Ensure log-file not in DEBUG mode.
 $sed -i 's/^    level: .*/    level: INFO  # one of: DEBUG INFO WARNING ERROR FATAL/' ${aio}/CO2MPAS/.co2_logconf.yaml.SAMPLE
+
+## Set Co2mpas test-key expiration 6monts:
+printf  'expire\n6m\nsave\n' | $gpg --batch --yes --command-fd 0 --status-fd 2 --edit-key 5464E04EE547D1FEDCAC4342B124C999CBBB52FF
+$GPG --rebuild-keydb-caches
+
+## Check keys...
+test_key="CBBB52FF"
+stamper_key="70B61F81"
+echo -e "\n\Inspect MANUALLY expiration, and no other keys than:\n    $test_key\n    $stamper_key"
+$GPG --allow-weak-digest-algos --list-keys
